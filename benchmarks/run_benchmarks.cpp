5d6c30eab82111d8c3167fcff0c9813e8a068d28
igor_rukhovich <ruhovich@mail.ru>
2020-05-08 19:44:52 +0300

    Run all benchmarks. Few of them need to rerun

--- benchmarks/run_benchmarks.cpp
+++ benchmarks/run_benchmarks.cpp
@@ -5,8 +5,8 @@
  */
 int main() {
     BenchFramework framework;
-    // framework.RunAllBenchmarks("../experiments/", BenchFramework::Range(1, 1'000'000, 35, true));
+    framework.RunAllBenchmarks("../experiments/", BenchFramework::Range(1, 1'000'000, 35, true));
     // framework.RunAllBenchmarks("../experiments/", BenchFramework::Range(0, 1'000'000, 200'000));
-    framework.RunAllBenchmarks("../experiments/", BenchFramework::Range(1, 100'000, 10, true));
+    // framework.RunAllBenchmarks("../experiments/", BenchFramework::Range(1, 100'000, 10, true));
     return 0;
 }
